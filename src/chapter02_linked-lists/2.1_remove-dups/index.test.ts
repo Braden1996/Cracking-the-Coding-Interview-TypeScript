e3181b559d4ca0ef0b1b140f08e205f966241b0f
Braden Marshall <braden1996@hotmail.co.uk>
2019-01-27 21:53:25 +0000

    :sparkles: Add Solution for Chapter 2 Exercise 1.

---
+++ src/chapter02_linked-lists/2.1_remove-dups/index.test.ts
@@ -0,0 +1,20 @@
+import each from 'jest-each';
+
+import LinkedList from '../utils/LinkedList';
+
+import * as solutions from './index';
+
+describe('Chapter 2 - 2.1 Remove Dups', () => {
+  Object.entries(solutions).forEach(([fnName, solution]) => {
+    describe(`solution ${fnName}`, () => {
+      each([
+        [LinkedList.fromChars('abbacdd'), LinkedList.fromChars('abcd')],
+      ]).test(
+        'with "%s" returns "%s"',
+        (testList: LinkedList<string>, expected: LinkedList<string>) => {
+          expect(solution(testList).toString()).toEqual(expected.toString());
+        },
+      );
+    });
+  });
+});
---
+++ src/chapter02_linked-lists/2.1_remove-dups/index.ts
@@ -0,0 +1,34 @@
+import LinkedList from '../utils/LinkedList';
+
+export function removeDups(list: LinkedList<string>) {
+  let lastNode = list;
+  const encounteredString = new Map<string, boolean>([[lastNode.data, true]]);
+
+  let curNode = list.next;
+  while (curNode !== null) {
+    if (encounteredString.get(curNode.data)) {
+      lastNode.next = curNode.next;
+    } else {
+      encounteredString.set(curNode.data, true);
+      lastNode = curNode;
+    }
+    curNode = curNode.next;
+  }
+  return list;
+}
+
+export function removeDups2(list: LinkedList<string>) {
+  let curNode: typeof list['next'] = list;
+  while (curNode !== null) {
+    let runnerNode = curNode;
+    while (runnerNode.next !== null) {
+      if (runnerNode.next.data === curNode.data) {
+        runnerNode.next = runnerNode.next.next;
+      } else {
+        runnerNode = runnerNode.next;
+      }
+    }
+    curNode = curNode.next;
+  }
+  return list;
+}
