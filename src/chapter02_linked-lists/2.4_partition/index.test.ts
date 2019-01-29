0725a8be038230908952cab27660edf8204949e3
Braden Marshall <braden1996@hotmail.co.uk>
2019-01-29 23:19:40 +0000

    :sparkles: Add Solution for Chapter 2 Exercise 4.

---
+++ src/chapter02_linked-lists/2.4_partition/index.test.ts
@@ -0,0 +1,32 @@
+import each from 'jest-each';
+
+import LinkedList from '../utils/LinkedList';
+
+import * as solutions from './index';
+
+describe('Chapter 2 - 2.4 Partition', () => {
+  Object.entries(solutions).forEach(([fnName, solution]) => {
+    describe(`solution ${fnName}`, () => {
+      each([
+        [
+          LinkedList.fromArray([3, 5, 7, 5, 10, 2, 1]),
+          5,
+          LinkedList.fromArray([1, 2, 3, 5, 7, 5, 10]),
+        ],
+        [LinkedList.fromArray([4, 4, 4]), 5, LinkedList.fromArray([4, 4, 4])],
+        [LinkedList.fromArray([6, 6, 6]), 5, LinkedList.fromArray([6, 6, 6])],
+      ]).test(
+        'with "%s" delete node "%s" returns "%s"',
+        (
+          testList: LinkedList<number>,
+          pivot: number,
+          expected: LinkedList<number>,
+        ) => {
+          expect(solution(testList, pivot)!.toString()).toEqual(
+            expected.toString(),
+          );
+        },
+      );
+    });
+  });
+});
---
+++ src/chapter02_linked-lists/2.4_partition/index.ts
@@ -0,0 +1,35 @@
+import LinkedList from '../utils/LinkedList';
+
+export function partition(list: LinkedList<number>, pivot: number) {
+  let leftListFirst: typeof list['next'] = null;
+  let leftListLast: typeof list['next'] = null;
+  let rightListFirst: typeof list['next'] = null;
+  let rightListLast: typeof list['next'] = null;
+
+  let curNode: typeof list['next'] = list;
+  while (curNode !== null) {
+    const nextNode: typeof curNode['next'] = curNode.next;
+    if (curNode.data < pivot) {
+      if (leftListFirst === null) {
+        leftListLast = curNode;
+      } else {
+        curNode.next = leftListFirst;
+      }
+      leftListFirst = curNode;
+    } else {
+      if (rightListLast === null) {
+        rightListFirst = curNode;
+      } else {
+        rightListLast.next = curNode;
+      }
+      rightListLast = curNode;
+    }
+    curNode = nextNode;
+  }
+
+  if (rightListLast !== null) rightListLast.next = null;
+  if (leftListLast === null) return rightListFirst;
+
+  leftListLast.next = rightListFirst;
+  return leftListFirst;
+}
