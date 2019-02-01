aa3436742aa8d0b08d7346c025f36efe883f79d9
Braden Marshall <braden1996@hotmail.co.uk>
2019-02-01 23:00:36 +0000

    :sparkles: Add Solution for Chapter 2 Exercise 6.

---
+++ src/chapter02_linked-lists/2.6_is-palindrome/index.test.ts
@@ -0,0 +1,23 @@
+import each from 'jest-each';
+
+import LinkedList from '../utils/LinkedList';
+
+import * as solutions from './index';
+
+describe('Chapter 2 - 2.6 Is Palindrome', () => {
+  Object.entries(solutions).forEach(([fnName, solution]) => {
+    describe(`solution ${fnName}`, () => {
+      each([
+        [LinkedList.fromChars('kayak'), true],
+        [LinkedList.fromChars('ayakk'), false],
+        [LinkedList.fromChars('redder'), true],
+        [LinkedList.fromChars('dderre'), false],
+      ]).test(
+        'with "%s" returns %s',
+        (list: LinkedList<number>, expected: boolean) => {
+          expect(solution(list)).toEqual(expected);
+        },
+      );
+    });
+  });
+});
---
+++ src/chapter02_linked-lists/2.6_is-palindrome/index.ts
@@ -0,0 +1,19 @@
+import LinkedList from '../utils/LinkedList';
+
+export function isPalindrome(list: LinkedList<number>) {
+  let head: typeof list['next'] = list;
+  let totalLength = 0;
+
+  function checkPalindromeRecursively(tail: typeof head) {
+    if (tail === null) return true;
+    totalLength++;
+    const trueSoFar = checkPalindromeRecursively(tail.next);
+    if (totalLength < 0) return true;
+    if (!trueSoFar || head!.data !== tail.data) return false;
+    totalLength += 2;
+    head = head!.next;
+    return true;
+  }
+
+  return checkPalindromeRecursively(head);
+}
