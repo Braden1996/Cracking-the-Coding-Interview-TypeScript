8ae6f8e576f479d36005e0ec2c682baf09ae260b
Braden Marshall <braden1996@hotmail.co.uk>
2019-02-02 21:29:35 +0000

    :sparkles: Add Solution for Chapter 2 Exercise 7.

---
+++ src/chapter02_linked-lists/2.7_intersection/index.test.ts
@@ -0,0 +1,27 @@
+import each from 'jest-each';
+
+import LinkedList from '../utils/TestFriendlyLinkedList';
+
+import * as solutions from './index';
+
+const scenario1 = LinkedList.fromChars('cde');
+const scenario1A = LinkedList.fromChars('ab') as LinkedList<string>;
+const scenario1B = LinkedList.fromChars('fgh') as LinkedList<string>;
+scenario1A.find(n => n.next === null)!.next = scenario1;
+scenario1B.find(n => n.next === null)!.next = scenario1;
+
+describe('Chapter 2 - 2.7 Intersection', () => {
+  Object.entries(solutions).forEach(([fnName, solution]) => {
+    describe(`solution ${fnName}`, () => {
+      each([
+        [scenario1A, scenario1B, scenario1],
+        [LinkedList.fromChars('ab'), LinkedList.fromChars('cd'), null],
+      ]).test(
+        'with "%s" and "%s" return %s',
+        (a: LinkedList<string>, b: LinkedList<string>, expected: boolean) => {
+          expect(solution(a, b)).toEqual(expected);
+        },
+      );
+    });
+  });
+});
---
+++ src/chapter02_linked-lists/2.7_intersection/index.ts
@@ -0,0 +1,29 @@
+import LinkedList from '../utils/LinkedList';
+
+function getLength<T>(node: LinkedList<T> | null) {
+  let count = 0;
+  while (node !== null) {
+    count++;
+    node = node.next;
+  }
+  return count;
+}
+
+export function getIntersection<T>(
+  a: LinkedList<T> | null,
+  b: LinkedList<T> | null,
+) {
+  const aLength = getLength(a);
+  const bLength = getLength(b);
+  const lengthDiff = aLength - bLength;
+
+  for (let i = 0; i < lengthDiff; i++) a = a!.next;
+  for (let i = lengthDiff; i < 0; i++) b = b!.next;
+
+  while (a !== b) {
+    a = a!.next;
+    b = b!.next;
+  }
+
+  return a;
+}
