86d035db20b0aac0c150a2a111605707e3b257dd
Braden Marshall <braden1996@hotmail.co.uk>
2019-02-02 21:55:12 +0000

    :sparkles: Add Solution for Chapter 2 Exercise 8.

---
+++ src/chapter02_linked-lists/2.8_loop-detection/index.test.ts
@@ -0,0 +1,26 @@
+import each from 'jest-each';
+
+import LinkedList from '../utils/TestFriendlyLinkedList';
+
+import * as solutions from './index';
+
+const scenario1 = LinkedList.fromChars('abc') as LinkedList<string>;
+const scenario1Loop = LinkedList.fromChars('defghijk') as LinkedList<string>;
+scenario1Loop.find(n => n.next === null)!.next = scenario1Loop;
+scenario1.find(n => n.next === null)!.next = scenario1Loop;
+
+describe('Chapter 2 - 2.8 Is Palindrome', () => {
+  Object.entries(solutions).forEach(([fnName, solution]) => {
+    describe(`solution ${fnName}`, () => {
+      each([
+        [scenario1, scenario1Loop],
+        [LinkedList.fromChars('abcdefghijk'), null],
+      ]).test(
+        'with test case %#',
+        (list: LinkedList<string>, expected: LinkedList<string> | null) => {
+          expect(solution(list)).toEqual(expected);
+        },
+      );
+    });
+  });
+});
---
+++ src/chapter02_linked-lists/2.8_loop-detection/index.ts
@@ -0,0 +1,22 @@
+import LinkedList from '../utils/LinkedList';
+
+export function returnLoopStart<T>(head: LinkedList<T> | null) {
+  let slow = head;
+  let fast = head;
+
+  while (fast !== null && fast.next !== null) {
+    slow = slow!.next;
+    fast = fast.next.next;
+    if (slow === fast) break;
+  }
+
+  if (fast === null || fast.next === null) return null;
+
+  slow = head;
+  while (slow !== fast) {
+    slow = slow!.next;
+    fast = fast!.next;
+  }
+
+  return fast;
+}
