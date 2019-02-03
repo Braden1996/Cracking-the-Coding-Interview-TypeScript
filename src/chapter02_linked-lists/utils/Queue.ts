7a7e9451283d4a65a5bbce553a0835436ae400db
Braden Marshall <braden1996@hotmail.co.uk>
2019-02-03 13:51:28 +0000

    :sparkles: Implement Queue with LinkedList.

---
+++ src/chapter02_linked-lists/utils/Queue.ts
@@ -0,0 +1,49 @@
+import LinkedList from './LinkedList';
+
+export default class Queue<T> {
+  static fromArray<T>(dataArr: T[]) {
+    const queue = new Queue<T>();
+    dataArr.forEach(d => queue.add(d));
+    return queue;
+  }
+
+  static fromChars(characters: string) {
+    return LinkedList.fromArray(characters.split(''));
+  }
+
+  private first: LinkedList<T> | null = null;
+  private last: LinkedList<T> | null = null;
+
+  toArray() {
+    return this.first ? this.first.toArray() : [];
+  }
+
+  toString() {
+    return this.toArray().reduce(
+      (str, n, i) => `${str}${i > 0 ? ' <- ' : ''}${n}`,
+      '',
+    );
+  }
+
+  add(data: T) {
+    const node = new LinkedList(data);
+    if (this.last !== null) this.last.next = node;
+    this.last = node;
+    if (this.first === null) this.first = this.last;
+  }
+
+  remove() {
+    const popped = this.first;
+    if (!popped) return null;
+    this.first = popped.next;
+    return popped.data;
+  }
+
+  peek() {
+    return this.isEmpty() ? null : this.first!.data;
+  }
+
+  isEmpty() {
+    return this.first === null;
+  }
+}
